3fa55094b457029f0f28aea65c0e61d45d9e7b27
sravanaytha <veerasravan123@gmail.com>
2023-05-10 19:49:56 +0530

    second commit

---
+++ server.ts
@@ -0,0 +1,23 @@
+const express = require('express')
+
+const server = new express()
+const port  = 2556
+
+server.get("/testsuccess",(req:any,res:any)=>{
+    res.status(200).json({
+        success:true,
+        message:"success message"
+    })
+    res.end()
+})
+
+server.get("/testfailure",(req:any,res:any)=>{
+    res.status(404).json({
+        success:true,
+        message:"failure message"
+    })
+    res.end()
+})
+server.listen(port,()=>{
+    console.log(`server runnning at ${port}`)
+})
