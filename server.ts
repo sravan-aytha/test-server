de02f46776e332d24898e82cee108f982fe6110a
sravanaytha <veerasravan123@gmail.com>
2023-05-10 20:23:03 +0530

    4

--- server.ts
+++ server.ts
@@ -1,5 +1,5 @@
-//const express = require('express')
-import express from 'express'
+const express = require('express')
+// import express from 'express'
 const server = express()
 const port  = 2556
 
